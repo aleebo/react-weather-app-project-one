66ed4c6b43098e1144170b1173ba3e6e1931abdf
aleebo <87545942+aleebo@users.noreply.github.com>
2021-11-28 00:14:14 -0800

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        {" "}
+        This project is coded by Alicia Vuong and is
+        <a
+          href="https://github.com/aleebo/react-weather-app-project-one"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on Github.
+        </a>
+      </footer>
+    </div>
+  );
 }
