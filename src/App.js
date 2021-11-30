ea7a0edc62414f5483cc317c547bbb21d02f8002
aleebo <87545942+aleebo@users.noreply.github.com>
2021-11-29 18:21:22 -0800

    update Weather JS & removed unneeded components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
+import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        {" "}
-        This project is coded by Alicia Vuong and is
-        <a
-          href="https://github.com/aleebo/react-weather-app-project-one"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <h1> Weather App</h1>
+        <Weather />
+        <footer>
           {" "}
-          open-sourced on Github.
-        </a>
-      </footer>
+          This project is coded by Alicia Vuong and is
+          <a
+            href="https://github.com/aleebo/react-weather-app-project-one"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,61 +1,50 @@
 import React from "react";
-import "./weather.css";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
+        <form>
+            <div className="row">
+            <div className="col-9">
+           
+            <input type="search"
+             placeholder="Enter a city..." 
+             autoFocus="on" 
+             className="form-control />
+           </div>
+           
+    
+           <div className="col-3">
+           <input type="submit" 
+            value="search" 
+            className="btn btn-primary" />
+    </div>
+    </div>
+        </form>
+
       <h1>New York</h1>
       <ul>
-        <li>Wed 7:00</li>
+        <li>Wednesday 7:00</li>
         <li> Mostly Cloudy</li>
       </ul>
 
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather64/partly_cloudy.png"
-              alt="Mostly Cloudy"
-            />
-            <div className="float-left">
-              <span className="temperture">
-                {" "}
-                6<span className="units">°C</span>{" "}
-              </span>
-
-              <div className="col-6">
-                <ul>
-                  <li>Precipitation: 15%</li>
-                  <li>Humidity: 72%</li>
-                  <li>Wind:13 m/h</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="weather">
-            <form />
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Enter a city.."
-                  autoFocus="on"
-                  className="form-control"
-                />
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="search"
-                    className="btn btn-primary"
-                    w-100
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="Mostly Cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind:13m/h</li>
+          </ul>
         </div>
       </div>
-    </div>
+    </div> 
   );
 }
